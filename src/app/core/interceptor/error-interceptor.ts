48e42289e1fd10da9b730e937065afffae04e947
elder <correo@correo.com>
2026-05-11 11:42:58 -0600

    Interceptores

---
+++ src/app/core/interceptor/error-interceptor.ts
@@ -0,0 +1,28 @@
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs'
+
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  return next(req).pipe(
+    catchError((error:HttpErrorResponse)=>{
+     
+      console.log("Estamos en el interceptor")
+
+      console.log(error.status);
+      
+      if(error.status === 0) {
+        console.error('Error de red', error.message)
+      } else if (error.status === 404) {
+        console.error('Recurso no encontrado', error.message)
+      } else if (error.status === 503) {
+        console.error('Error interno del servidor', error.message)
+      } else {
+        console.error('Error desconocido', error.message)
+      }
+      
+
+      return throwError(()=> error)
+
+    })
+  );
+};
--- src/app/core/service/producto.ts
+++ src/app/core/service/producto.ts
@@ -1,14 +1,14 @@
 import { Injectable, inject } from '@angular/core';
 import { ApiResponse } from '@modules/productos/models/product.models'
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 import { environment } from '@environments/environment'
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductoService {
-  private apiUrl = `${environment.apiUrl}/api/shoppingcart/public-product`;
+  private apiUrl = `${environment.apiUrl}/api/shoppingcart/public-products`;
   // private apiUrl = `/api/shoppingcart/public-products`;
 
   // constructor(private http: HttpClient) {
@@ -20,5 +20,7 @@ export class ProductoService {
   public getAllProducts(): Observable<ApiResponse> {
     return this.http.get<ApiResponse>(this.apiUrl)
   }
+
+
 
 }
