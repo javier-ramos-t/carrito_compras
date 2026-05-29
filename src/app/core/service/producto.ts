8098343f6e6f40ff8c3873aa3b1da3f8ed64a1b6
Javier Ramos <javier.irt.021700@outlook.com>
2026-05-29 12:15:24 -0600

    Correccion de rutas para el CRUD de productos

--- src/app/core/service/producto.ts
+++ src/app/core/service/producto.ts
@@ -8,7 +8,7 @@ import { environment } from '@environments/environment'
   providedIn: 'root',
 })
 export class ProductoService {
-  private apiUrl = `${environment.apiUrl}/api/shoppingcart/public-products/`;
+  private apiUrl = `${environment.apiUrl}/api/shoppingcart/products`;
 
   private http = inject(HttpClient)
 
@@ -23,15 +23,15 @@ export class ProductoService {
   }
 
   public createProduct(productData: ProductRequest): Observable<ApiResponse>{
-    return this.http.post<ApiResponse>(this.apiUrl, productData)
+    return this.http.post<ApiResponse>(`${this.apiUrl}/`, productData)
   }
 
   public updateProduct(productData: ProductUpdate, id: number): Observable<ApiResponse>{
-    return this.http.put<ApiResponse>(`${this.apiUrl}${id}/`, productData)
+    return this.http.put<ApiResponse>(`${this.apiUrl}/${id}/`, productData)
   }
 
   public deleteProduct(id: number): Observable<ApiResponse>{
-    return this.http.delete<ApiResponse>(`${this.apiUrl}${id}/`)
+    return this.http.delete<ApiResponse>(`${this.apiUrl}/${id}/`)
   }
 
 
